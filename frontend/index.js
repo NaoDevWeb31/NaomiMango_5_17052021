1e81c39fc912ce029afedfb2714853f1dc8f039d
Naomi Mango <naomi.mango@gmail.com>
2021-05-17 18:17:41 +0200

    [MAJ] Fonction pour appeler l'API et récupérer les produits dans index.js

--- frontend/index.js
+++ frontend/index.js
@@ -10,7 +10,19 @@ async function main(){
 }
 
 function getProducts(){
-
+    // Appel de l'API via fetch pour recevoir les 5 produits et leurs infos
+    return fetch(urlApi)
+        .then(function(response){
+            // Récupérer la réponse en JSON
+            return response.json();
+        })
+        .then(function(products) {
+            return products;
+        })
+        .catch(function(error){
+            // Une erreur s'est produit
+            alert("Les produits sont indisponibles pour le moment, veuillez nous excusez pour la gêne occasionnée");
+    });
 }
 
 function fillPage(products){
