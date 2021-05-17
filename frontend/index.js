1e24d839e7fbfcf7044817ee7af98c9d0ab6c877
Naomi Mango <naomi.mango@gmail.com>
2021-05-17 18:10:44 +0200

    [NEW] Index.js + [MAJ] Script index.js dans la page d'index

---
+++ frontend/index.js
@@ -0,0 +1,18 @@
+const urlApi = "http://localhost:3000/api/teddies";
+
+main()
+
+async function main(){
+}
+
+function getProducts(){
+
+}
+
+function fillPage(products){
+
+}
+
+function showProduct(product){
+
+}
