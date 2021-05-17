e4bc75ffffe652fc373ed8849d2d399b2181a497
Naomi Mango <naomi.mango@gmail.com>
2021-05-17 18:12:32 +0200

    [MAJ] Fonction principale de index.js

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,10 @@ const urlApi = "http://localhost:3000/api/teddies";
 main()
 
 async function main(){
+    // Attendre l'appel de l'API via fetch pour les produits
+    const products = await getProducts();
+    // Remplir la page des produits dès que l'API a transmis les infos
+    fillPage(products)
 }
 
 function getProducts(){
@@ -14,5 +18,5 @@ function fillPage(products){
 }
 
 function showProduct(product){
-
+    
 }
