63535021db2cbcd86107f60e22b662b917149354
Naomi Mango <naomi.mango@gmail.com>
2021-05-18 18:56:36 +0200

    [NEW] Création de cart.js + [MAJ] Script cart.js dans la page de panier

---
+++ frontend/js/cart.js
@@ -0,0 +1,38 @@
+const urlApi = "http://localhost:3000/api/teddies/";
+
+// Récupérer les infos envoyées dans le panier (transformé de JSON en objet JS et stocké dans le navigateur)
+let getCart = JSON.parse(localStorage.getItem("cart"));
+
+main()
+
+async function main(){
+    
+}
+
+function fillPage(){
+
+}
+
+function getCartList(){
+
+}
+
+function getCartProductData(){
+
+}
+
+function showTotalCartAmount(){
+
+}
+
+function totalCartAmount(){
+
+}
+
+function formCheck(){
+
+}
+
+function sendOrder(){
+    
+}
