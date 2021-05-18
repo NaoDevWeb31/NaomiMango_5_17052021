aab49f7c72176793ee72de17b031479e893490af
Naomi Mango <naomi.mango@gmail.com>
2021-05-18 12:00:07 +0200

    [NEW] Création de product.js + [MAJ] Script product.js dans la page de produit

---
+++ frontend/js/product.js
@@ -0,0 +1,23 @@
+const urlApi = "http://localhost:3000/api/teddies/";
+
+main()
+
+async function main(){
+
+}
+
+function getProductId(){
+
+}
+
+function getProductData(productId){
+
+}
+
+function fillPage(product){
+
+}
+
+function addToShoppingCart(product){
+    
+}
