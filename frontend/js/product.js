7991b8622e36c28f5e25d5c1ccb4a89159493c7f
Naomi Mango <naomi.mango@gmail.com>
2021-05-18 12:02:25 +0200

    [MAJ] Fonction principale de product.js

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -3,7 +3,12 @@ const urlApi = "http://localhost:3000/api/teddies/";
 main()
 
 async function main(){
-
+    // Attendre d'avoir l'id du produit
+    const productId = await getProductId();
+    // Attendre l'appel de l'API via fetch pour recevoir les données du produit
+    const productData = await getProductData(productId);
+    // Remplir la page des données du produit dès qu'on a son id et que l'API a transmis les infos
+    fillPage(productData)
 }
 
 function getProductId(){
@@ -19,5 +24,5 @@ function fillPage(product){
 }
 
 function addToShoppingCart(product){
-    
+
 }
