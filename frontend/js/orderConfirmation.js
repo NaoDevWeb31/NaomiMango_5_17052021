b1001febe1e73b2fe3e6dcf9414ead1924b80c7b
Naomi Mango <naomi.mango@gmail.com>
2021-05-19 12:19:51 +0200

    [MAJ] Fonction pour afficher les infos de la commande dans orderConfirmation.js

--- frontend/js/orderConfirmation.js
+++ frontend/js/orderConfirmation.js
@@ -8,7 +8,17 @@ const contact
 const orderAmount
 
 function showOrderData(){
-
+    // Récupérer le template de la page de confirmation
+    const templateElt = document.getElementById("orderDataTemplate");
+    // Cloner le template
+    const cloneTempElt = document.importNode(templateElt.content, true);
+    // Remplir chaque élément du clone du template
+    cloneTempElt.getElementById("orderId").textContent = orderId;
+    cloneTempElt.getElementById("customerFirstName").textContent = contact.firstName;
+    cloneTempElt.getElementById("orderAmount").textContent = orderAmount + ",00 €";
+    cloneTempElt.getElementById("customerEmail").textContent = contact.email;
+    // Afficher le clone du template à l'endroit souhaité
+    document.getElementById("mainContent").appendChild(cloneTempElt);
 }
 
 showOrderData()
