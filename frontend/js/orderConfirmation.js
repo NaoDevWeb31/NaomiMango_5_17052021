1e474cbafa5f7cd5d2c31a03b06c6463c1a74fa9
Naomi Mango <naomi.mango@gmail.com>
2021-05-19 12:18:23 +0200

    [NEW] Création de orderConfirmation.js

---
+++ frontend/js/orderConfirmation.js
@@ -0,0 +1,14 @@
+// Récupérer l'identifiant de la commande
+const orderId
+
+// Récupérer les infos du client de la commande stocké dans le navigateur
+const contact
+
+// Récupérer le montant de la commande stocké dans le navigateur
+const orderAmount
+
+function showOrderData(){
+
+}
+
+showOrderData()
