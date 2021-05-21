35fa5f5e249200d67ac6653933cb840fab24a035
Naomi Mango <naomi.mango@gmail.com>
2021-05-21 13:42:57 +0200

    [NEW] Création d'une classe Teddy sur teddy.js + [MAJ] Lien sur product.html et cart.html

---
+++ frontend/js/teddy.js
@@ -0,0 +1,11 @@
+class Teddy {
+    constructor({name, imageUrl, _id, price, description, colors, quantity}){
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this._id = _id;
+        this.price = price;
+        this.description = description;
+        this.colors = colors;
+        this.quantity = quantity;
+    }
+}
